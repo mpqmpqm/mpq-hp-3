b83a786f5c38a01372fa570e531bd4ba6e91f315
Mason Pilcher <mpq@mpq.dev>
2020-10-10 15:41:40 -0400

    Init. Init live cv, cv styling

---
+++ components/pages/work/resume/Resume.js
@@ -0,0 +1,22 @@
+import styles from "./Resume.module.css"
+
+const Resume = ({ html }) => {
+  return (
+    <>
+      <div
+        dangerouslySetInnerHTML={{ __html: html }}
+        className={styles.container}
+      />
+      <style global jsx>
+        {`
+          h3 + p.dates {
+            margin-top: 0;
+            font-style: italic;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Resume
---
+++ pages/work/resume.js
@@ -0,0 +1,35 @@
+import showdown from "showdown"
+import { Octokit } from "@octokit/core"
+import Resume from "../../components/pages/work/resume/Resume"
+import styles from "../../styles/pages/resume.module.css"
+
+const resume = ({ markdown }) => {
+  const converter = new showdown.Converter()
+  const html = converter.makeHtml(markdown)
+  return (
+    <main className={styles.page}>
+      <Resume {...{ html }} />
+    </main>
+  )
+}
+
+export default resume
+
+export async function getStaticProps() {
+  const octokit = new Octokit({ auth: process.env.GITHUB_KEY })
+
+  const res = await octokit.request(
+    "GET /repos/mpqmpqm/resume/contents/Resume.md",
+    {
+      owner: "mpqmpqm",
+      repo: "resume",
+      type: "private",
+    }
+  )
+
+  return {
+    props: {
+      markdown: Buffer.from(res.data.content, "base64").toString(),
+    },
+  }
+}
