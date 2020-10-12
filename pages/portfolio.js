c63a5d4ece3f4599086c2aad506e6114b61c6568
Mason Pilcher <mpq@mpq.dev>
2020-10-11 21:23:23 -0400

    /work -> /portfolio

---
+++ pages/portfolio.js
@@ -0,0 +1,32 @@
+import Layout from "../components/Layout"
+import Link from "next/link"
+import { fetchGithubLinks } from "../utils/fetchGithub"
+
+const portfolio = ({ links }) => {
+  console.log(links)
+  return (
+    <Layout>
+      <main>
+        {links.map(({ path, title }) => (
+          <Link href={`/${path}`} key={path}>
+            <a>{title}</a>
+          </Link>
+        ))}
+      </main>
+    </Layout>
+  )
+}
+
+export default portfolio
+
+export async function getStaticProps() {
+  return {
+    props: {
+      links: await fetchGithubLinks({
+        repo: `resume`,
+        path: `portfolio`,
+        pathsOnly: false,
+      }),
+    },
+  }
+}
---
+++ pages/portfolio/[project].js
@@ -0,0 +1,43 @@
+import { fetchGithubLinks, fetchGithubMarkdown } from "../../utils/fetchGithub"
+import { markdownToReact } from "../../utils/markdownToReact"
+import Layout from "../../components/Layout"
+
+const project = ({ content, title }) => {
+  return (
+    <Layout subPath={title}>
+      <main>{markdownToReact({ content })}</main>
+    </Layout>
+  )
+}
+
+export default project
+
+export async function getStaticProps(context) {
+  const { project } = context.params
+  const {
+    content,
+    data: { title },
+  } = await fetchGithubMarkdown({
+    repo: `resume`,
+    path: `/portfolio/${project}.md`,
+  })
+  return {
+    props: {
+      content,
+      title,
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      ...(await fetchGithubLinks({
+        repo: `resume`,
+        path: `portfolio`,
+        pathsOnly: true,
+      })),
+    ],
+    fallback: false,
+  }
+}
