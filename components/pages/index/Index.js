32ce00b478c409f571d2afd1ffcf6ab26b990b98
Mason Pilcher <mpq@mpq.dev>
2020-11-06 14:42:57 -0500

    Move all content into markdown

--- components/pages/index/Index.js
+++ components/pages/index/Index.js
@@ -2,12 +2,7 @@ import { markdownToReact } from "../../../utils/markdownToReact"
 import styles from "./Index.module.css"
 
 const Index = ({ content }) => {
-  return (
-    <main className={styles.main}>
-      <h1>My name is MPQ 👋</h1>
-      {markdownToReact({ content })}
-    </main>
-  )
+  return <main className={styles.main}>{markdownToReact({ content })}</main>
 }
 
 export default Index
