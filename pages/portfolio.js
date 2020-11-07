4f1c38105400e8cfd7ec643dfc1993141b0eab14
Mason Pilcher <mpq@mpq.dev>
2020-11-07 12:59:09 -0500

    accept markdown content

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,18 +1,21 @@
 import Layout from "../components/Layout"
 import Link from "next/link"
 import styles from "../styles/pages/portfolio.module.css"
-import { fetchGithubLinks } from "../utils/fetchGithub"
+import { fetchGithubMarkdown, fetchGithubLinks } from "../utils/fetchGithub"
+import { markdownToReact } from "../utils/markdownToReact"
 
-const portfolio = ({ links }) => {
+const portfolio = ({ content, links }) => {
   return (
     <Layout>
       <main>
-        <h1 className={styles.h1}>Selected projects</h1>
+        {markdownToReact({ content })}
         {links.map(({ path, title, tags }) => (
           <div className={styles.project} key={path}>
-            <Link href={`/${path}`}>
-              <a>{title}</a>
-            </Link>
+            <h2 className={styles.title}>
+              <Link href={`/${path}`}>
+                <a>{title}</a>
+              </Link>
+            </h2>
             <p className={styles.tags}>
               <i>Tags:</i> {tags}
             </p>
@@ -26,6 +29,11 @@ const portfolio = ({ links }) => {
 export default portfolio
 
 export async function getStaticProps() {
+  const { content } = await fetchGithubMarkdown({
+    repo: `resume`,
+    path: `Portfolio.md`,
+  })
+
   return {
     props: {
       links: await fetchGithubLinks({
@@ -33,6 +41,7 @@ export async function getStaticProps() {
         path: `portfolio`,
         pathsOnly: false,
       }),
+      content,
     },
   }
 }
