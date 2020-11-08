845df1f5cb4422a19ddc780ee593f17930c107f7
Mason Pilcher <mpq@mpq.dev>
2020-11-08 12:29:04 -0500

    mobile fixed nav, init useFixedNav

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import PageNav from "./PageNav"
+import useFixedNav from "../hooks/useFixedNav"
 
 const Layout = ({ children, subPath }) => {
+  const menuHeight = useFixedNav()
   return (
     <>
       <PageNav {...{ subPath }} />
-      {children}
+      <div style={{ marginBottom: menuHeight }}>{children}</div>
     </>
   )
 }
---
+++ hooks/useFixedNav.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react"
+
+const useFixedNav = () => {
+  const [menuHeight, setMenuHeight] = useState(0)
+
+  useEffect(() => {
+    const media = window.matchMedia(`(min-width: 699px)`)
+
+    const listener = () => {
+      if (!media.matches) {
+        const nav = document.querySelector(`nav`)
+        const { height } = nav.getBoundingClientRect()
+        setMenuHeight(height * 1.25)
+      } else setMenuHeight(0)
+    }
+
+    listener()
+    media.addEventListener(`change`, listener)
+
+    return () => {
+      media.removeEventListener(`change`, listener)
+    }
+  }, [])
+
+  return menuHeight
+}
+
+export default useFixedNav
