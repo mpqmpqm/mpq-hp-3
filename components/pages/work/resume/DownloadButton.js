3db211b2b6657a4347b2a54db401cad43e327aa8
MPQ <mpq@mpq.dev>
2021-06-25 23:16:14 -0400

    Use button over link for file download

--- components/pages/work/resume/DownloadButton.js
+++ components/pages/work/resume/DownloadButton.js
@@ -1,16 +1,42 @@
 import { useState } from "react"
 import styles from "./DownloadButton.module.css"
+import Spinner from "../../../Spinner"
 
 const DownloadButton = ({ fetchPath }) => {
+  const [fetching, setFetching] = useState(false)
+  const handleClick = async () => {
+    setFetching(true)
+    try {
+      const blob = await fetch(fetchPath, {
+        responseType: "arraybuffer",
+        headers: {
+          Accept: "application/pdf",
+        },
+      }).then(async (res) => await res.blob())
+
+      const link = document.createElement("a")
+      link.href = window.URL.createObjectURL(blob)
+      link.download = "MPQ_Resume.pdf"
+      link.click()
+    } catch (e) {
+      console.error(e)
+      alert(
+        `Something went wrong. Please try again, or use your browser's native print dialog.`
+      )
+    }
+    setFetching(false)
+  }
   return (
-    <a
-      className={`${styles.anchor} online-only`}
-      title="Download resume"
-      href={fetchPath}
-      // download
-    >
-      Download (PDF)
-    </a>
+    <div className={styles.container}>
+      <button
+        onClick={handleClick}
+        className={styles.button}
+        disabled={fetching}
+      >
+        Download
+      </button>
+      {fetching && <Spinner color="var(--bright-purple)" size="1em" reverse />}
+    </div>
   )
 }
 
