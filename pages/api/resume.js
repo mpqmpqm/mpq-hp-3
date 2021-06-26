8d56ceac7fa66e770b63ba54f41a1e4b36aa2cb9
MPQ <mpq@mpq.dev>
2021-06-25 21:58:49 -0400

    use chrome-aws-lambda

--- pages/api/resume.js
+++ pages/api/resume.js
@@ -1,8 +1,15 @@
-import puppeteer from "puppeteer"
+// import puppeteer from "puppeteer-core"
+import chromium from "chrome-aws-lambda"
 
 export default async function returnResume(req, res) {
   const pdf = await (async function printPDF() {
-    const browser = await puppeteer.launch({ headless: true })
+    const browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    })
     const page = await browser.newPage()
     await page.goto("https://mpq.dev/resume", { waitUntil: "networkidle0" })
     const pdf = await page.pdf({
