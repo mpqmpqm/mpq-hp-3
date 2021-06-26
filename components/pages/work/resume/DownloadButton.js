1d4ca88ba4d1121d566fe80da0a6ab2295f0cb65
MPQ <mpq@mpq.dev>
2021-06-25 20:47:02 -0400

    init download button

---
+++ components/pages/work/resume/DownloadButton.js
@@ -0,0 +1,15 @@
+import { useState } from "react"
+import styles from "./DownloadButton.module.css"
+
+const DownloadButton = ({ fetchPath }) => (
+  <a
+    className={`${styles.anchor} online-only`}
+    title="Download resume"
+    href={fetchPath}
+    download
+  >
+    Download (PDF)
+  </a>
+)
+
+export default DownloadButton
